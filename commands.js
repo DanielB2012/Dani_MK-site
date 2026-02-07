d76cc3f9b1a44a09eebb82432d83b084cad57223
DanielB2012 <daniel.a.bernard@free.fr>
2026-02-07 19:08:44 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -5,7 +5,7 @@ const BATCHES = {};
 const MAX_BATCH_NAME = 50;
 
 // ----------------- API Paste.ee -----------------
-const PASTEE_API_KEY = "u2Wb3I904XespqCYJ2bQCUbwhMuQR4qBvoWKxOOxD";
+const PASTEE_API_KEY = "ublF9EMLX5fKhYd6X2gBJjzlSzGPEqxUSwjbWwpek";
 const PASTEE_API_URL = "https://api.paste.ee/v1/pastes";
 
 // ----------------- UTILITAIRES -----------------
@@ -237,6 +237,7 @@ async function runCommand(input, callback) {
 // ----------------- EXPORT -----------------
 window.runCommand = runCommand;
 window.getJump = getJump;
+
 
 
 
